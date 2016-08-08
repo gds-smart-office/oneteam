481bcb2db9a22ba913ac11a6a4413916d7423d7c
Poh Kah Kong <kahkong87mail@gmail.com>
2016-08-08 13:15:35 +0800

    First commit

---
+++ bamboo.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'json'
+require 'rest-client'
+
+plan = 'TR-CIUT'
+url = "http://www.ida-gds.com/bamboo/rest/api/latest/result/#{plan}.json?expand=results[0].result&expand=results.result.jiraIssues"
+user = 'poh_kah_kong'
+password = 'p@ssw0rd'
+while true
+  json = JSON.parse(RestClient::Request.execute method: :get, url: url, user: user, password: password)
+  p json['results']['result'][0]['state']
+  sleep 5
+end
