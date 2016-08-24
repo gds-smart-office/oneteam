9aa960cee25e68503ad35906a31db2aa11ff783a
Poh Kah Kong <kahkong87mail@gmail.com>
2016-08-24 22:06:43 +0800

    Update bamboo.rb

--- bamboo.rb
+++ bamboo.rb
@@ -7,12 +7,10 @@ include PiPiper
 
 plan = 'TR-ARRP'
 url = "http://www.ida-gds.com/bamboo/rest/api/latest/result/#{plan}.json?expand=results[0].result&expand=results.result.jiraIssues"
-user = 'poh_kah_kong'
-password = 'p@ssw0rd'
 successful_pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
 failed_pin     = PiPiper::Pin.new(:pin => 18, :direction => :out)
 while true
-  json = JSON.parse(RestClient::Request.execute method: :get, url: url, user: user, password: password)
+  json = JSON.parse(RestClient::Request.execute method: :get, url: url, user: ENV['BAMBOO_USER'], password: ENV['BAMBOO_PASSWORD'])
   status = json['results']['result'][0]['state']
   if status == 'successful'
     puts "#{Time.now}: Successful!"
